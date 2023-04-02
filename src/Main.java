7624402b60d4bf641e39afb80616f941f08ebd5b
Pedro Márcio <pedromos16@gmail.com>
2023-04-02 15:08:28 -0300

    Trabalho finalizado com a documentação necessária.
    Para acelerar, comentar a parte 1 (linha 25), tempo com a parte 1 pode ser superior a uma hora

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,53 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Enunciado:
+ * Estudamos, durante as aulas da disciplina, como a complexidade de tempo (custo de execução) de um
+ * algoritmo pode impactar o tempo para a obtenção de uma solução para um problema computacional.
+ * O cálculo matemático da complexidade do algoritmo dá uma medida segura para compararmos
+ * algoritmos e seus comportamentos.
+ * Neste exercício, vamos comparar, na prática, as operações realizadas por dois algoritmos diferentes
+ * para o mesmo problema: ordenação. Iremos, ainda, tentar perceber que, utilizando dados aleatórios,
+ * a média das execuções tende para a média entre os melhores e piores casos.
+ * A descrição completa das tarefas segue:
+ * PRIMEIRA PARTE
+ * a) Crie um método para gerar um vetor contendo 100.000 valores inteiros aleatórios, sem
+ * repetição.
+ * b) Crie uma cópia do vetor gerado em (a).
+ * c) Utilize o método da bolha para ordenar o vetor original, contando quantas operações de
+ * comparação de troca e quantas operações de comparação foram realizadas.
+ * d) Utilize o método da seleção para ordenar o a cópia (b) do vetor, contando quantas
+ * operações de comparação de troca e quantas operações de comparação foram realizadas.
+ * e) Repita as operações de (a) até (d) 50 vezes e para este conjunto de testes, registre para cada
+ * um dos dois algoritmos:
+ * • Maior, menor e média da quantidade de comparações realizadas;
+ * • Maior, menor e média da quantidade de trocas realizadas;
+ * f) Criar uma tabela e um gráfico comparando os resultados dos dois algoritmos.
+ * g) Analisar criticamente esta tabela, utilizando os conhecimentos que você tem sobre os
+ * algoritmos.
+ * SEGUNDA PARTE
+ * a) Utilize um método similar ao da primeira parte para gerar um vetor contendo 10.000 valores
+ * inteiros, iniciando em 1 e indo até 10.000 na ordem.
+ * b) Sorteie um número entre 1 e 11.000 e faça a busca sequencial por este número no vetor,
+ * registrando em qual posição ele foi encontrado (use a posição 10.001 caso ele não exista).
+ * c) Repita os passos (a) e (b) 1 milhão de vezes.
+ * d) Agrupe os resultados dos contadores de 500 em 500. Explicando: todos os contadores entre
+ * 1 e 500 devem ser somados, depois os de 501 a 1000 e assim por diante.
+ * e) Faça um gráfico de barras com os resultados obtidos em (d) e comente o resultado.
+ * Ressaltando: o documento a ser entregue deve iniciar com as especificações do equipamento e
+ * depois, organizadamente e de forma completa, deve apresentar os testes e seus resultados. Tão
+ * importante do quantos os testes são a organização do resultado, sua apresentação e seus
+ * comentários.
+ */
+
 public class Main {
+
+    /**
+     * Decalaração de variáveis e vetores globais para me auxiliar na captação
+     * e tratativa dos dados
+     */
+
     static long qtdTrocasBolha = 0;
     static long qtdCompBolha = 0;
     static long qtdTrocasSelecao = 0;
@@ -17,12 +63,12 @@ public class Main {
     static long[] totalAleatorio = new long[22]; //ou 21
 
     public static void main(String[] args) throws Exception {
-        //long tempoInicial = System.currentTimeMillis();
         long total = 0;
-        //List<Integer> gerado = geraVetor(100_000), a = gerado, b = gerado;
-        //a = new LinkedList<>(gerado);
-        //b = new LinkedList<>(gerado);
+
         /**
+         * Nesse for eu gero 50 vezes um vetor aleatório e faço sua ordenação, separadamente
+         * pelo Selection Sort e pelo Bubble Sort
+         */
         for (int i = 0; i < 50; i++) {
             List<Integer> gerado = geraVetor(100_000, true), a = gerado;
             a = new LinkedList<>(gerado);
@@ -32,6 +78,13 @@ public class Main {
         }
 
         System.out.print("Parte 1:");
+
+        /**
+         * Nessa sequencia de Prints, eu realizo a ordenação dos dados coletados nos algoritmos
+         * de seleção e bolha, usando o Merge Sort para uma maior velocidade, para poder saber o
+         * menor e maior valor, além de sua média. Está sendo impresso pelo tipo de método e dentro
+         * de cada um, imprime também a as comparações e trocas
+         */
 
         mergeSort(totalCompBolha);
         System.out.println();
@@ -72,7 +125,8 @@ public class Main {
         }
         System.out.println("Media: " + total / totalTrocaSelecao.length);
 
-        /**
+        /*
+        // testando se está funcionandoS
         System.out.println();
         for (int i = 0; i < totalCompBolha.length; i++) {
             System.out.print(totalCompBolha[i]+" ");
@@ -89,23 +143,38 @@ public class Main {
         for (int i = 0; i < totalTrocaSelecao.length; i++) {
             System.out.print(totalTrocaSelecao[i]+" ");
         }
-
-         **/
+         */
 
         System.out.println("Parte 2: ");
 
+        /**
+         * Gero um número aleatório a cada iteração do for, no caso 1.000.000 de vezes,
+         * e também um vetor ordenado a cada iteração. Criei uma variável "categoria"
+         * para me auxiliar a saber em qual espaço entre os valores de 500 o valor vai
+         * estar. Implementei uma busca sequencial como foi requisitado e um método
+         * "agrupamentoRecursivo" para usar a "categoria" e me retornar em qual espaço
+         * de 500 o valor está. Finalmente, somando os valores correspondentes em um
+         * vetor "totalAleatorio" para ter os valores finais
+         */
+
+
+        //Para uma testagem mais rápida e eficaz, recomendo colocar o 1.000.000 em 1.000 ou 10.000
         Random aleatorio = new Random();
         int numAle=0, posicao = 0, categoria = 0;
         for (int i = 0; i < 1_000_000; i++) {
             categoria = 0;
             numAle = aleatorio.nextInt(11_000)+1;
-            List<Integer> gerado = geraVetor(10_000, false), a = gerado;
-            a = new LinkedList<>(gerado);
+            List<Integer> gerado = geraVetor(10_000, false);
             Integer[] y = gerado.toArray(new Integer[0]);
             posicao = pesquisaSequencial(numAle, y);
             categoria = agrupamentoRecursivo(1,500, posicao, categoria);
             totalAleatorio[categoria] = totalAleatorio[categoria] + posicao;
         }
+
+        /**
+         * Imprime na tela todos os espaços requistados entre valores de 500 em 500, partindo
+         * de 1 e 500, com o valor total da soma de suas posições
+         */
 
         int max=500, min=1;
         for (int i = 0; i < totalAleatorio.length; i++) {
@@ -113,9 +182,6 @@ public class Main {
             max += 500;
             min += 500;
         }
-
-        //long tempoFinal = System.currentTimeMillis();
-        //System.out.println(tempoFinal-tempoInicial);
     }
 
     /**
@@ -235,7 +301,6 @@ public class Main {
     /* Variavel utilizada para guarda a posição onde os
       valores serão inseridos no novo vetor. */
         int pos = 0;
-
     /* Enquanto o inicio não chegar até o meio do vetor, ou o meio do vetor
       não chegar até seu fim, compara os valores entre o inicio e o meio,
       verificando em qual ordem vai guarda-los ordenado.*/
@@ -283,21 +348,29 @@ public class Main {
      *
      * @param x    - Número que será procurado.
      * @param numeros - Vetor de números.
+     * @return cont ou 10001 - A posição que o número está
      */
     static public Integer pesquisaSequencial(int x, Integer[] numeros) {
         int cont = 0;
-
         for(cont = 0; cont < numeros.length; cont++) {
-            //Verifica se o elemento que está sendo procurado está no vetor.
             if (numeros[cont] == x) {
-                //Se encontrou o elemento, imprime ele na tela e para a pesquisa.
                 return cont;
 
             }
         }
-
         return 10001;
     }
+
+    /**
+     * Método recursivo para alocar o valor na catégoria correta, seja entre
+     * 1 e 500, 501 e 1000 e etc.
+     *
+     * @param min - limite mínimo (valor inicial: 1)
+     * @param max - Limite máximo (valor inicial: 500)
+     * @param posicao - Posição que o valor foi encontrado no vetor
+     * @param categoria - Divisões criadas para permitir o agrupamento fácil dos dados
+     * @return categoria que o valor se encontra
+     */
 
     static public int agrupamentoRecursivo(int min, int max, int posicao, int categoria){
         if (posicao>= min && posicao<=max) {
